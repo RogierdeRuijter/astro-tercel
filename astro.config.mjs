cd538e343bb679028f05518365a5977281e5b7f2
Rogier de Ruijter <r.de.ruijter@vpro.nl>
2025-01-30 14:38:33 +0100

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
+import redirects from './redirects.json' assert { type: 'json' };
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  redirects: {
-    'my-redirect': '/',
-    "/blog/[...slug]": "/articles/[...slug]"
-  }
+  redirects,
 });
